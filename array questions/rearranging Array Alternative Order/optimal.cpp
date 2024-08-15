25560fb9387aa51247bf63c79bcb7c3a85aa0ee3
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-15 23:14:35 +0530

    optimal for rearrangement question

--- array questions/rearranging Array Alternative Order/optimal.cpp
+++ array questions/rearranging Array Alternative Order/optimal.cpp
@@ -1,35 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> rearrangeArray(vector<int> &nums){
+vector<int> rearrangeArray(vector<int> &nums)
+{
     vector<int> ans(nums.size(), 0);
     int posiIndex = 0;
     int negiIndex = 1;
 
     for (int i = 0; i < nums.size(); i++)
     {
-        if(nums[i] < 0){
+        if (nums[i] < 0)
+        {
             ans[negiIndex] = nums[i];
             negiIndex += 2;
         }
-        else{
+        else
+        {
             ans[posiIndex] = nums[i];
             posiIndex += 2;
         }
     }
 
     return ans;
-    
 }
 
-int main(){
-    vector<int> nums = {3,1,-2,-5,2,-4};
+int main()
+{
+    vector<int> nums = {3, 1, -2, -5, 2, -4};
     vector<int> arrangedArray = rearrangeArray(nums);
 
-    cout<<"Rearranged array is : ";
+    cout << "Rearranged array is : ";
 
-    for (int i = 0; i < arrangedArray.size(); i++) {
+    for (int i = 0; i < arrangedArray.size(); i++)
+    {
         cout << arrangedArray[i] << " ";
     }
     cout << endl;
