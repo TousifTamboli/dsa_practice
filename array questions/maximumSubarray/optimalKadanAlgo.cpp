33c193d543aad80b19d4e6963b31647e064a4ee9
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-15 15:30:37 +0530

    optimal approach kadan's algorithm O(n)

--- array questions/maximumSubarray/optimalKadanAlgo.cpp
+++ array questions/maximumSubarray/optimalKadanAlgo.cpp
@@ -2,25 +2,37 @@
 
 using namespace std;
 
-int maximumSubarray(vector<int> &arr)
+long long maximumSubarray(vector<int> &arr)
 {
 
-    int maxSoFar = arr[0];
-    int maxEndingHere = arr[0];
+    long long maxi = LONG_MIN;
+    long long sum = 0;
 
-    for (int i = 1; i < arr.size(); i++)
+    for (int i = 0; i < arr.size(); i++)
     {
-        maxEndingHere = max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = max(maxSoFar, maxEndingHere);
+        sum += arr[i];
+
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+
+        // sum of Empty array if mentioned
+        //  if(maxi < 0) maxi = 0;
     }
 
-    return maxSoFar;
+    return maxi;
 }
 
 int main()
 {
     vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int maxSum = maximumSubarray(arr);
+    long long maxSum = maximumSubarray(arr);
     cout << "The maximum subarray sum is: " << maxSum << endl;
     return 0;
 }
