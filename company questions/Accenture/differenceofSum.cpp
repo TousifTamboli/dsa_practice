6c945169df050749654639629bf015f3f8e75dcb
Tousif Tamboli <tousiftamboli3@gmail.com>
2025-04-06 01:14:12 +0530

    largeSmallSum

--- company questions/Accenture/differenceofSum.cpp
+++ company questions/Accenture/differenceofSum.cpp
@@ -1,40 +1,49 @@
-// // The function def differenceofSum(n. m) accepts two integers n, m as arguments 
-// Find the sum of all numbers in range from 1 to m(both inclusive) that are not divisible by n. 
-// Return difference between sum of integers not divisible by n with sum of numbers divisible by n.
+// The function accepts an integers arr of size ’length’ as its arguments you are required to
+// return the sum of second largest largest element from the even positions and second smallest
+//  from the odd position of given ‘arr’.
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int differenceofSum(int n, int m)
-{
-    int sumOfN = 0;
-    int sumOfM = 0;
+int largeSmallSum(vector<int> &a) {
 
-    for (int i = 1; i <= m; i++)
-    {
-        if (i % n == 0)
-        {
-            sumOfN += i;
+    int n = a.size();
+
+    if(n == 0) return 0;
+    if(n <= 3) return 0;
+
+    vector<int> even;
+    vector<int> odd;
+
+    for(int i=0; i<n; i++){
+        //even
+        if(i % 2 == 0){
+            even.push_back(a[i]);
         }
-        else
-            sumOfM += i;
+        //odd
+        else{
+            odd.push_back(a[i]);
+        }
     }
 
-    int diff = sumOfM - sumOfN;
+    if(even.size() < 2 || odd.size() < 2) return -1;
 
-    return diff;
+    sort(even.begin(), even.end());
+    sort(odd.begin(), odd.end());
+
+    int ans = even[even.size()-2] + odd[1];
+
+    return ans;
+
+
 }
 
 int main()
 {
-    int n = 3;
-    int m = 10;
+    // vector<int> a = {3, 2, 1, 7, 5, 4};
+    vector<int> a = {1, 8, 0, 2, 3, 5, 6};
 
-    int result;
-
-    // cin >> n >> m;
-
-    result = differenceofSum(n, m);
+    int result = largeSmallSum(a);
 
     cout << result;
     return 0;
